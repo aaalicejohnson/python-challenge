a42e554854788f355c94cf8cd6886a6f5c20d1d0
Alice Johnson <aaalicejohnson@gmail.com>
2022-09-29 01:36:38 -0400

    code changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,27 @@ import csv
 
 csvpath = os.path.join('Resources','budget_data.csv')
 
-months = []
-profit_loss = []
+profit = []
+profit_change = []
+
+total_months = 0
+total_profit_change = 0 
+monthly_profit_change = 0
+
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    
+    csv_header = next(csvreader)
 
-    print(csvreader)
+    for row in csvreader:
+        total_months =+ 1
+        total_profit_change =+ int(row[1])
+
+        profit.append(row[1])
+
+            
+    print("Months:", total_months)
+    print(total_profit_change)
+    print(profit)
+
