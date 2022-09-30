5b588c782760d32ca3d4502953f446533c9991ed
Alice Johnson <aaalicejohnson@gmail.com>
2022-09-30 14:34:04 -0400

    Final PyBank Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,14 @@ import os
 import csv
 
 csvpath = os.path.join('Resources','budget_data.csv')
+output_path = os.path.join('Analysis', 'analysis.txt')
 
 profit = []
 profit_change = []
+date = []
 
 total_months = 0
-total_profit_change = 0 
-monthly_profit_change = 0
-
+initial_profit = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -18,13 +18,64 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
     for row in csvreader:
-        total_months =+ 1
-        total_profit_change =+ int(row[1])
+        total_months = total_months + 1
 
-        profit.append(row[1])
+        monthly_profit = int(row[1])
+        monthly_profit_change = monthly_profit - initial_profit
+        
+        profit.append(monthly_profit)
+        date.append(row[0])
+        profit_change.append(monthly_profit_change)
 
-            
-    print("Months:", total_months)
-    print(total_profit_change)
-    print(profit)
+        initial_profit = monthly_profit
+    
+a = profit_change.pop(0)
+b = date.pop(0)
+
+pl_data = zip(date, profit_change)
+
+sum_change = sum(profit_change)
+
+total_profit = sum(profit)
+
+average_change = float(sum_change / len(profit_change))
+
+max_profit = max(profit_change)
+min_profit = min(profit_change)
+
+for month, change in pl_data:
+    if change == max_profit:
+        max_month = month
+    if change == min_profit:
+        min_month = month 
+
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: " + str(total_months))
+print("Total: $" +str(total_profit))
+print(f"Average Change: ${average_change:.2f}") 
+print(f"Greatest Increase in Profits: {max_month} (${max_profit})")
+print(f"Greatest Decrease in Profits: {min_month} (${min_profit})")
+
+with open(output_path, "w") as txtfile:
+    txtfile.write("Financial Analysis")
+    txtfile.write("\n")
+    txtfile.write("----------------------------")
+    txtfile.write("\n")
+    txtfile.write("Total: $" +str(total_profit))
+    txtfile.write("\n")
+    txtfile.write(f"Average Change: ${average_change:.2f}") 
+    txtfile.write("\n")
+    txtfile.write(f"Greatest Increase in Profits: {max_month} (${max_profit})")
+    txtfile.write("\n")
+    txtfile.write(f"Greatest Decrease in Profits: {min_month} (${min_profit})")
+
+
+
+
+
+
+
+
+
 
